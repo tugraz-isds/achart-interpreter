1eb9215519f771c0097d8b1fa54a7e4d3ffc3cf8
Christopher Kopel <chriskindl@gmail.com>
2021-04-20 08:40:52 +0200

    Updates

--- asummarise.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var achart_summariser_1 = require("./achart-summariser/achart-summariser");
-var achart_summariser = new achart_summariser_1.AChartSummariser();
